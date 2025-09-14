e8e938c098922f5f4d228af87d2308b356e60cde
Danilo Jesús <danilojesus112001@gmail.com>
2025-09-14 20:56:58 -0300

    feat: criando o modal para a página

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,24 +1,82 @@
 import Section from '../Section'
-import { Items, Item } from './styles'
+import { Items, Item, Action, Modal, ModalContent } from './styles'
 
-const Gallery = () => {
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
+
+type GalleryItem = {
+  type: 'video' | 'image'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: hogwarts
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') {
+      return item.url
+    }
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') {
+      return zoom
+    }
+    return play
+  }
+
   return (
-    <Section title="Galeria" background="black">
-      <Items>
-        <Item>
-          <img src="https://placehold.co/150" alt="Imagem" />
-        </Item>
-        <Item>
-          <img src="https://placehold.co/150" alt="Imagem" />
-        </Item>
-        <Item>
-          <img src="https://placehold.co/150" alt="Imagem" />
-        </Item>
-        <Item>
-          <img src="https://placehold.co/150" alt="Imagem" />
-        </Item>
-      </Items>
-    </Section>
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={index}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Media ${index + 1} da ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a midia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={fechar} alt="Icone de fechar" />
+          </header>
+          <img src={defaultCover} alt={name} />
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -6,12 +6,71 @@ export const Items = styled.ul`
   gap: 16px;
 `
 
+export const Action = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background-color: #000000c0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+`
+
 export const Item = styled.li`
-  img {
+  position: relative;
+
+  > img {
     border: 2px solid ${cores.branca};
     border-radius: 8px;
     width: 150px;
     height: 150px;
     object-fit: cover;
+  }
+
+  &:hover {
+    ${Action} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+  }
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #000000c0;
+`
+
+export const ModalContent = styled.div`
+  max-width: 960px;
+  position: relative;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: bold;
+    }
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
   }
 `
