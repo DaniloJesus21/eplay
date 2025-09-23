a2c6fbe33620ffad86168da2d12b1dc4aeb4c32e
Danilo Jesús <danilojesus112001@gmail.com>
2025-09-23 15:58:11 -0300

    feat: criando o carrinho

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,42 @@
+import Button from '../Button'
+import Tag from '../Tag'
+
+import {
+  CartContainer,
+  CartItem,
+  Overlay,
+  Prices,
+  Quantity,
+  Sidebar
+} from './styles'
+
+const Cart = () => {
+  return (
+    <CartContainer>
+      <Overlay />
+      <Sidebar>
+        <ul>
+          <CartItem>
+            <img src="https://placehold.co/80x80" alt="" />
+            <div>
+              <h3>Nome do jogo</h3>
+              <Tag>RPG</Tag>
+              <Tag>PS5</Tag>
+              <span>R$ 150,00</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+        </ul>
+        <Quantity>2 jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total: R$ 250,00 <span>em até 6x sem juros</span>
+        </Prices>
+        <Button type="button" title="Clique aqui para continuar com a compra">
+          Continuar com a compra
+        </Button>
+      </Sidebar>
+    </CartContainer>
+  )
+}
+
+export default Cart
